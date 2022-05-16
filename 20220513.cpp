053b18429532867bdcf4844a39ead3d9bd080f9b
ildar151086 <71975751+ildar151086@users.noreply.github.com>
2022-05-16 19:59:06 +0500

    struct

---
+++ 20220513.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+
+struct ListT {
+    float Value;
+    ListT* next;
+};
+
+ListT* top = nullptr;
+
+void push(float valueUser) {
+    ListT* temp = new ListT;
+    temp->Value = valueUser;
+    temp->next = top;
+    top = temp;
+};
+
+void show() {
+    ListT* temp = top;
+    while (temp != nullptr) {
+        cout << temp->Value;
+        temp = temp->next;
+    }
+}
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    ListT listArr;
+
+    for (int i = 0; i < 10; i++) {
+        push(i);
+    };
+    show();
+}
